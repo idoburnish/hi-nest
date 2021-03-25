53331288bd808505b23d960e1ae6de2f7d3c8248
idoburnish <69456626+idoburnish@users.noreply.github.com>
2021-03-25 15:30:52 +0900

    update

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll(@Req() req, @Req() res): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get(":id")
+    getOne(@Param('id') movieId: number): Movie {
+        console.log(typeof movieId);
+        return this.moviesService.getOne(movieId);
+    } 
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete(":id") 
+    remove(@Param('id') movieId: number) {
+        return this.moviesService.deleteOne(movieId);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto){
+        return this.moviesService.update(movieId, updateData);
+    }
+}
